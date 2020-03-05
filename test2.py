9e684c6046d8b174c6829e78bd4aa83b982bc52e
Shirakami Emmmer <yjhdandan@163.com>
2020-03-05 22:01:39 +0800

    test

--- test2.py
+++ test2.py
@@ -1,18 +1,16 @@
-
-f = open("test.ncm","rb")
-
+f = open("./test.ncm","rb")
 
 file_name = "out.mp3"
-    m = open("out.mp3",'wb')
-    chunk = bytearray()
-    while True:
-        chunk = bytearray(f.read(0x8000))
-        chunk_length = len(chunk)
-        if not chunk:
-            break
-        for i in range(1,chunk_length+1):
-            j = i & 0xff;
-            chunk[i-1] ^= key_box[(key_box[j] + key_box[(key_box[j] + j) & 0xff]) & 0xff]
-        m.write(chunk)
-    m.close()
-    f.close()
+m = open("./out.mp3",'wb')
+chunk = bytearray()
+while True:
+    chunk = bytearray(f.read(0x8000))
+    chunk_length = len(chunk)
+    if not chunk:
+        break
+    for i in range(1,chunk_length+1):
+        j = i & 0xff;
+        chunk[i-1] ^= key_box[(key_box[j] + key_box[(key_box[j] + j) & 0xff]) & 0xff]
+    m.write(chunk)
+m.close()
+f.close()
